b222f54c61c9b448d67a94bbc63a81f53b99ac76
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-08-28 22:49:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const User=require('./db/User');
 const app=express(); 
 app.use(cors());  
 app.use(express.json());
-
+ 
 app.post("/register",async (req,res)=>{
     let user=new User(req.body);
     let result=await user.save(); 
