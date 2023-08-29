4ad78089e1d25215c7b97f34b0e43baf9ed5c1fc
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-08-29 19:35:08 +0530

    a

---
+++ db/Product.js
@@ -0,0 +1,10 @@
+const mongoose=require('mongoose');
+
+const productSchema=new mongoose.Schema({
+    name:String,
+    price:String,
+    category:String,
+    userId:String,
+    company:String
+});
+module.exports=mongoose.model('Products',productSchema);
