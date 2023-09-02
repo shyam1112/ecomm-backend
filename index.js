46188ba48e80f31301689a782d3c0bc0997b9c41
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-09-02 13:21:05 +0530

    update

--- index.js
+++ index.js
@@ -68,6 +68,17 @@ app.put("/products/:id",async (req,res)=>{
         }
     )
     res.send(result);
-})
+});
+
+app.get("/search/:key", async (req,res)=>{
+    let result=await Product.find({
+         "$or":[
+            {name:{$regex:req.params.key}},
+            {company:{$regex:req.params.key}},
+            {category:{$regex:req.params.key}}
+         ]
+    })
+    res.send(result);
+});
 
 app.listen(5000);   
