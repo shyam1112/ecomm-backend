156ad80d0c73657bca9ff939c63249a115705447
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-09-02 10:29:20 +0530

    up

--- index.js
+++ index.js
@@ -37,5 +37,37 @@ app.post("/addproduct",async (req,res)=>{
     res.send(result);
 })
 
+app.get("/products",async (req,res)=>{
+    let products= await Product.find();
+    if(products.length>0){
+        res.send(products);
+    }else{
+        res.send({result:"No products found."});
+    }
+});
+
+app.delete("/products/:id",async (req,res)=>{
+    const result=await Product.deleteOne({_id:req.params.id});
+    res.send(result);
+});
+
+app.get("/products/:id",async (req,res)=>{
+    const result=await Product.findOne({_id:req.params.id});
+    if(result){
+        res.send(result);
+    }else{
+        res.send({result:"No data found."});
+    }
+});
+
+app.put("/products/:id",async (req,res)=>{
+    const result=await Product.updateOne(
+        {_id:req.params.id},
+        {
+            $set:req.body
+        }
+    )
+    res.send(result);
+})
 
 app.listen(5000);   
