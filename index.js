ae3726502f3ab71a52a8da61e33d143e8c95a91a
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-09-20 21:48:29 +0530

    q

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const express=require('express');
 require('./db/config');
 const cors=require('cors');
-const User=require('./db/user');
+const User=require('./db/userr');
 const Product = require('./db/product');
 const Cart=require('./db/cart');
 const Order =require('./db/order')
