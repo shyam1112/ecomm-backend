38f5dcab62846f0e0e3c552754157567a7417b85
shyam sardhara <80083773+shyam1112@users.noreply.github.com>
2023-09-23 19:17:36 +0530

    tt

--- index.js
+++ index.js
@@ -64,16 +64,23 @@ app.post('/order', async (req, res) => {
     }
   });
   
-  app.get("/order",async (req,res)=>{
-    let order= await Order.find();
-    if(order.length>0){
-        res.send(order);
-    }else{
-        res.send({result:"No products found."});
+  app.get("/profile/:id", async (req, res) => {
+    try {
+      const result = await User.findOne({ _id: req.params.id });
+  
+      if (!result) {
+        return res.status(404).json({ error: "User not found" });
+      }
+  
+      res.json(result);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Internal server error" });
     }
-});
+  });
 
 app.delete("/order/:id",async (req,res)=>{
+
     const result=await Order.deleteOne({_id:req.params.id});
     res.send(result);
 });
